1381743c7d92fba5cd0111d7a68207eea2e08cd8
sss <sss@sssnoMacBook-Pro.local>
2022-07-13 00:17:33 +0900

    [add]Adminサインアップ/ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
   get 'users/show'
+
+
+  resources :admins, only: [:index]
+  # devise_for :admins
+  devise_for :admins, controllers: { registrations: 'admins/registrations', sessions: 'admins/sessions' }
   devise_for :users
   root 'profile#show'
 end
