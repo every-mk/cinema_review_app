adaa7b51e971dced92cdea03ff8e472ade9b8434
sss <sss@sssnoMacBook-Pro.local>
2022-07-18 17:35:20 +0900

    [add]Profileモデルテストケース追加

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :profile do
+    name        { "ユーザー" }
+    sex         { 0 }
+    date_of_birth { "2022/07/01" }
+    prefecture    { "沖縄県" }
+    municipality  { "那覇市" }
   end
 end
