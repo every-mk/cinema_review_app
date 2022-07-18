064d78a7f6154ce82b27e9bcb606c71c10845a05
sss <sss@sssnoMacBook-Pro.local>
2022-07-18 17:38:51 +0900

    [fix]インデント調整

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :profile do
-    name        { "ユーザー" }
-    sex         { 0 }
+    name          { "ユーザー" }
+    sex           { 0 }
     date_of_birth { "2022/07/01" }
     prefecture    { "沖縄県" }
     municipality  { "那覇市" }
