87b2e452a3191b7621b2685100fbabfd7981ba84
sss <sss@sssnoMacBook-Pro.local>
2022-07-21 02:14:23 +0900

    [change]サインアップ画面のエラー遷移先を指定

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,28 +2,173 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   let(:user) { create(:user, profile: profile) }
+  let(:build_user) { build(:user, email: "build_user@gmail.com", profile: profile1) }
   let(:profile) { build(:profile) }
+  let(:profile1) { build(:profile) }
 
   before do
     profile.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'profile', 'default_icon.jpeg')), filename: 'default_icon.jpeg', content_type: 'image/jpeg')
     user.confirm
   end
 
+  describe "#sign_up" do
+    before do
+      visit new_user_registration_path
+    end
+
+    it "when displayed, the header is displayed correctly" do
+      expect(page).to have_selector "img[alt=サイトのロゴ画像]"
+      # ToDo: サイトのログ画像URLを追加する.
+      expect(page).to have_content "ログイン"
+      expect(page).to have_content "登録する"
+    end
+
+    it "when displayed, the content is displayed correctly" do
+      expect(page).to have_selector 'p', text: "新しいアカウントを作成"
+      expect(page).to have_content "名前"
+      expect(page).to have_content "性別"
+      expect(page).to have_content "男性"
+      expect(page).to have_content "女性"
+      expect(page).to have_content "生年月日"
+      expect(page).to have_content "都道府県"
+      expect(page).to have_content "Eメール"
+      expect(page).to have_content "新しいアカウントを作成"
+      expect(page).to have_selector "input[value='新しいアカウントを作成']"
+      expect(page).to have_content "パスワード"
+      expect(page).to have_content "パスワード（確認用）"
+      expect(page).to have_content "既にアカウントをお持ちの方はログイン"
+      expect(page).to have_content "認証確認メールが届かない場合"
+    end
+
+    it "when displayed, the footer is displayed correctly" do
+      expect(page).to have_content "©︎ 2022 S.K"
+    end
+
+    scenario "when all fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+
+      expect do
+        click_button "新しいアカウントを作成"
+        expect(page).to have_content "本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。"
+      end.to change(User, :count).by(1)
+    end
+
+    scenario "when name is not fill, created account" do
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "名前を入力してください"
+    end
+
+    scenario "when date of birth is not fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "生年月日を入力してください"
+    end
+
+    scenario "when prefecture is fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "都道府県を入力してください"
+    end
+
+    scenario "when municipality is fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "市区町村を入力してください"
+    end
+
+    scenario "when email is fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "Eメールを入力してください"
+    end
+
+    scenario "when password is fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password_confirmation", with: build_user.password_confirmation
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "パスワードを入力してください"
+    end
+
+    scenario "when password confirmation is fill, created account" do
+      fill_in "profile_name", with: profile.name
+      choose "女性"
+      fill_in "profile_date_of_birth", with: profile.date_of_birth
+      fill_in "profile_prefecture", with: profile.prefecture
+      fill_in "profile_municipality", with: profile.municipality
+      fill_in "user_email", with: "build_user1@gmail.com"
+      fill_in "user_password", with: build_user.password
+      click_button "新しいアカウントを作成"
+      expect(page).to have_content "パスワード（確認用）とパスワードの入力が一致しません"
+    end
+
+    scenario "when you login click the link, login will be displayed" do
+      click_link "既にアカウントをお持ちの方はログイン"
+      expect(page).to have_selector 'p', text: "ログイン"
+    end
+
+    scenario "when you resend authentication confirmation email click the link, resend authentication confirmation email will be displayed" do
+      click_link "認証確認メールが届かない場合"
+      expect(page).to have_selector 'p', text: "認証確認メールが届かない場合"
+    end
+  end
+
   describe "#account" do
     before do
       sign_in user
       visit users_account_path
-      allow(controller).to receive(:current_user).and_return(user)
     end
 
-    it 'when displayed, the header is displayed correctly' do
+    it "when displayed, the header is displayed correctly" do
       expect(page).to have_selector "img[alt=サイトのロゴ画像]"
       # ToDo: サイトのログ画像URLを追加する.
       expect(page).to have_selector "img[alt=アイコン画像]"
       expect(page).to have_selector "img[src='#{url_for(profile.image.attachment)}']"
     end
 
-    it 'when displayed, the content is displayed correctly' do
+    it "when displayed, the content is displayed correctly" do
       expect(page).to have_selector 'li', text: "アカウント"
       expect(page).to have_selector 'li', text: "プロファイル"
       expect(page).to have_selector 'p', text: "アカウント"
@@ -35,36 +180,36 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_content "アカウント削除"
     end
 
-    it 'when displayed, the footer is displayed correctly' do
+    it "when displayed, the footer is displayed correctly" do
       expect(page).to have_content "©︎ 2022 S.K"
     end
 
-    scenario 'when you pc version account click the link, the account information will be displayed' do
+    scenario "when you pc version account click the link, the account information will be displayed" do
       find("div[class='user-nav user-nav-pc']").click_on "アカウント"
       expect(page).to have_selector 'p', text: "アカウント"
     end
 
-    scenario 'when you mobile version account click the link, the account information will be displayed' do
+    scenario "when you mobile version account click the link, the account information will be displayed" do
       find("div[class='user-nav user-nav-mobile']").click_on "アカウント"
       expect(page).to have_selector 'p', text: "アカウント"
     end
 
-    scenario 'when you pc version profile click the link, the profile information will be displayed' do
+    scenario "when you pc version profile click the link, the profile information will be displayed" do
       find("div[class='user-nav user-nav-pc']").click_on "プロファイル"
       expect(page).to have_selector 'p', text: "プロファイル"
     end
 
-    scenario 'when you mobile version profile click the link, the profile information will be displayed' do
+    scenario "when you mobile version profile click the link, the profile information will be displayed" do
       find("div[class='user-nav user-nav-mobile']").click_on "プロファイル"
       expect(page).to have_selector 'p', text: "プロファイル"
     end
 
-    scenario 'when you edit click the link, the account edit will be displayed' do
+    scenario "when you edit click the link, the account edit will be displayed" do
       click_link "編集"
       expect(page).to have_selector 'h1', text: "アカウント編集"
     end
 
-    scenario 'when you account delete click the link, account deleted', js: true do
+    scenario "when you account delete click the link, account deleted", js: true do
       click_link "アカウント削除"
       expect(page.accept_confirm).to eq "本当に削除しますか？"
       expect(page).to have_content "アカウントを削除しました"
