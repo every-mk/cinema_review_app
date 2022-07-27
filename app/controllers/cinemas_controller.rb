b04a4e4aed3d369920a4eed509a439d3978ee298
sss <sss@sssnoMacBook-Pro.local>
2022-07-27 18:47:09 +0900

    [fix]cinema#showでログインしてない場合に表示されない不具合対応

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -34,7 +34,7 @@ class CinemasController < ApplicationController
   end
 
   def show
-    if current_user.review.exists?(cinema_id: @cinema.id)
+    if current_user.present? && current_user.review.exists?(cinema_id: @cinema.id)
       @review = Review.find_by(user_id: current_user.id, cinema_id: @cinema.id)
     else
       @review = nil
